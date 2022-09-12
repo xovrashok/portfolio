40a412dcd9ce14e98e62cfe7a50c374b059fa5f9
xovrasok <yuliaruda033@gmail.com>
2022-09-12 13:13:47 +0300

    add skills

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,10 +1,48 @@
 import React from "react";
+import Me from "./Me.jpg";
+import { Image, Container } from "./styles";
+import Typography from "@mui/material/Typography";
 
 const Main = () => {
   return (
-    <div>
-      <img src="src/components/Icons/Me.jpg" alt="Me" />
-    </div>
+    <Container>
+      <Image src={Me} alt="me" />
+      <div style={{ marginLeft: "50px", fontSize: "18px", fontWeight: "300" }}>
+        <Typography paragraph>
+          Hi!
+          <br />
+          My name is Julia and I'm a web developer based out of Kharkov.
+        </Typography>
+        <Typography paragraph>
+          My professional experience includes two years of software development,
+          with focus on Web Applications and sophisticated web solution in the
+          last few years.
+        </Typography>
+        <Typography paragraph>
+          I have had an opportunity to work with a variety of technologies so I
+          can offer a comprehensive approach to Web development.
+        </Typography>
+        <Typography paragraph>SKILLSET</Typography>
+        <Typography paragraph>
+          <ul>
+            Backend:
+            <li> Languages: JavaScript/TypeScript, NodeJS </li>
+            <li> Backend Web Frameworks: Express </li>
+            <li> APIs: REST, GraphQL </li>
+            <li> Understanding of Design Patterns and Software Architecture</li>
+          </ul>
+        </Typography>
+        <Typography paragraph>
+          <ul>
+          Frontend:
+            <li> Languages: Javascript + HTML5/CSS </li>
+            <li> React and associated libraries (Redux) </li>
+            <li> Angular - Testing: Jest, Enzyme </li>
+            <li> UI Libraries: Material UI, Bootstrap, MUI</li>
+          </ul>
+        </Typography>
+      </div>
+    </Container>
   );
 };
 
---
+++ src/components/Main/styles.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Image = styled.img`
+  width: 406px;
+  border-radius: 227px;
+  box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%),
+    0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
+  float: left;
+  margin-right: 45px;
+`;
+
+export const Container = styled.div`
+`;
