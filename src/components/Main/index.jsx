1c41266af3870ef28edf8f4c5812f1300ef1168b
xovrasok <yuliaruda033@gmail.com>
2022-09-13 11:38:11 +0300

    information about me

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,29 +1,38 @@
 import React from "react";
 import Me from "./Me.jpg";
-import { Image, Container } from "./styles";
+import {
+  Image,
+  Container,
+  Text,
+  ImageContainer,
+  TextContainer,
+} from "./styles";
 import Typography from "@mui/material/Typography";
 
 const Main = () => {
   return (
     <Container>
-      <Image src={Me} alt="me" />
-      <div style={{ marginLeft: "50px", fontSize: "18px", fontWeight: "300" }}>
-        <Typography paragraph>
-          Hi!
-          <br />
-          My name is Julia and I'm a web developer based out of Kharkov.
-        </Typography>
-        <Typography paragraph>
+      <ImageContainer>
+        <Image src={Me} alt="me" />
+        <Text>
+          My name is Julia <br />
+          and I'm a web developer <br /> based out of Kharkov.
+        </Text>
+      </ImageContainer>
+      <TextContainer style={{ fontSize: "18px", fontWeight: "300" }}>
+        <Typography paragraph sx={{ textAlign: "justify" }}>
           My professional experience includes two years of software development,
           with focus on Web Applications and sophisticated web solution in the
           last few years.
         </Typography>
-        <Typography paragraph>
+        <Typography paragraph sx={{ textAlign: "justify" }}>
           I have had an opportunity to work with a variety of technologies so I
           can offer a comprehensive approach to Web development.
         </Typography>
-        <Typography paragraph>SKILLSET</Typography>
-        <Typography paragraph>
+        <Typography paragraph sx={{ textAlign: "justify" }}>
+          SKILLSET
+        </Typography>
+        <Typography paragraph sx={{ textAlign: "justify" }}>
           <ul>
             Backend:
             <li> Languages: JavaScript/TypeScript, NodeJS </li>
@@ -32,16 +41,16 @@ const Main = () => {
             <li> Understanding of Design Patterns and Software Architecture</li>
           </ul>
         </Typography>
-        <Typography paragraph>
+        <Typography paragraph sx={{ textAlign: "justify" }}>
           <ul>
-          Frontend:
+            Frontend:
             <li> Languages: Javascript + HTML5/CSS </li>
             <li> React and associated libraries (Redux) </li>
             <li> Angular - Testing: Jest, Enzyme </li>
             <li> UI Libraries: Material UI, Bootstrap, MUI</li>
           </ul>
         </Typography>
-      </div>
+      </TextContainer>
     </Container>
   );
 };
--- src/components/Main/styles.jsx
+++ src/components/Main/styles.jsx
@@ -1,13 +1,36 @@
 import styled from "styled-components";
 
 export const Image = styled.img`
-  width: 406px;
-  border-radius: 227px;
+  width: 340px;
+  border-radius: 405px;
   box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%),
     0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
-  float: left;
-  margin-right: 45px;
 `;
 
 export const Container = styled.div`
+  width: 400px;
+  border-radius: 10px;
+  overflow: hidden;
+  margin: 0 auto;
+`;
+
+export const TextContainer = styled.div`
+  background: #f3f3f3;
+  padding: 30px;
+  border-radius: 0 0 10px 10px;
+  margin: -10px;
+`;
+
+export const ImageContainer = styled.div`
+  background-color: #c35b5b;
+  text-align: center;
+  padding: 20px;
+  position: relative;
+  border-radius: 10px;
+`;
+
+export const Text = styled.text`
+  color: white;
+  font-size: 25px;
+  text-shadow: 1px 1px 2px cadetblue;
 `;
